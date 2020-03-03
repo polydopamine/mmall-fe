7d8efcfe82435d550295d8f20cce0812c8c4bbc9
wsh <wangsihangcc@163.com>
2020-03-03 22:11:20 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: wsh
+* @Date:   2020-03-02 21:42:09
+* @Last Modified by:   wsh
+* @Last Modified time: 2020-03-03 17:24:42
+*/
+// 使用严格模式
+'use strict';
+
+require('../module.js');
+require('./index.css');
+console.log('hello index');
---
+++ src/page/login/login.js
@@ -0,0 +1,9 @@
+/*
+* @Author: wsh
+* @Date:   2020-03-03 16:31:09
+* @Last Modified by:   wsh
+* @Last Modified time: 2020-03-03 17:05:05
+*/
+'use strict';
+require('../module.js');
+console.log('hello login');
